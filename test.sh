9ab33653376b5dfe1fb398a8b4ec14f3328e084c
Clove335 <guc335jpn@gmail.com>
2019-03-21 18:06:52 +0900

    Add tokenizer.

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@ try() {
   expected="$1"
   input="$2"
 
-  ./9cc "$input" > tmp.s
-  gcc -o tmp tmp.s
-  ./tmp
+  ./9cc "$input" > tmp2.s
+  gcc -o tmp2 tmp2.s
+  ./tmp2
   actual="$?"
   
   if [ "$actual" = "$expected" ]; then
@@ -18,5 +18,6 @@ try() {
 try 0 0
 try 42 42
 try 21 '5+20-4'
+try 41 " 12 + 34 - 5 " 
 
 echo OK
