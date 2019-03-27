c37be825ce969440b352a88917253921876573bc
Clove335 <guc335jpn@gmail.com>
2019-03-27 22:40:49 +0900

    Add abstract syntax tree and code generator.

--- 9cc.c
+++ 9cc.c
@@ -9,6 +9,17 @@ enum {
   TK_EOF,
 };
 
+enum {
+  ND_NUM = 256,   /* Type of int Node  */
+};
+
+typedef struct Node {
+  int ty;             /* Operator or ND_NUM */
+  struct Node *lhs;   /* Left-hand side */
+  struct Node *rhs;   /* Right-hand side */
+  int val;            /* Use only when ty is ND_NUM */
+} Node;
+
 typedef struct {
   int ty;       //Type of Token
   int val;      //tyがTKNUMの場合、その数値
@@ -16,6 +27,7 @@ typedef struct {
 } Token;
 
 Token tokens[100];
+int pos;
 
 void tokenize(char *p) {
   int i = 0;
@@ -50,57 +62,81 @@ void tokenize(char *p) {
   tokens[i].input = p;
 }
 
-/* Function that outputs the error. */
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+int consume(int ty) {
+  if (tokens[pos].ty != ty) {
+    return 0;
+  }
+  pos++;
+  return 1;
+}
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push%d\n", node->val);
+    return;
+  }
+  
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  mul rdi\n");
+    break;
+  case '/':
+    printf("  mov rdx, 0\n");
+    printf("  div rdi\n");
+  }
+
+  printf("  push rax\n");
+}
+
 void error(int i) {
   fprintf(stderr, "予期しないトークンです: %s\n",
-          tokens[i].input);
+      tokens[i].input);
   exit(1);
 }
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません");
+    fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
-  
+  /* Tokenize and parse. */
   tokenize(argv[1]);
   
+  /* Output the first half of Assembly. */
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  
-  /* Check if the first input is digit to verify the equatation. */
-  if (tokens[0].ty != TK_NUM) {
-    error(0);
-  }
-  printf("  mov rax, %d\n", tokens[0].val);
-  
-  /* Output the assembly. */
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
 
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-    
-    error(i);
-  }
 
+  /* Load the value of all equatation to RAX and return it. */
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
