8fe1778660897faff9239ed1b72ac4c708f2aa63
Clove335 <guc335jpn@gmail.com>
2019-03-29 00:40:34 +0900

    Add '(', ')'.

--- 9cc.c
+++ 9cc.c
@@ -43,7 +43,7 @@ void tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-' || *p == '*' || *p == '/') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
